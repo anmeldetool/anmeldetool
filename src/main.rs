08c3991a93cd7e70bd6af92ce6c570151f943c0f
rafael <rafael@schiafone.de>
2022-12-29 19:15:39 +0100

    basic rocket setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use rocket_dyn_templates::{context, Template};
+
+use utils::{App, Plugin};
+
+use plugin_user_basic::Users;
+
+#[rocket::launch]
+async fn rocket() -> _ {
+    let app = App::new();
+    let plugins: Vec<Box<dyn Plugin>> = vec![Box::new(Users::new())];
+
+    // plugins.iter().for_each(|plugin| plugin.init(&app).unwrap());
+
+    rocket::build()
+        .mount("/", rocket::routes![index])
+        .attach(Template::fairing())
+}
+
+#[rocket::get("/")]
+async fn index() -> Template {
+    Template::render("index", context! { field: "Hello World" })
 }
